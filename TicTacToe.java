c55669d7a6bc235ca4b10b9b61fa1929b9536ac2
Sean Grindal <sean37gl@gmail.com>
2022-10-14 22:45:25 -0400

    swtich game state

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,5 +1,31 @@
 public class TicTacToe {
+  int[] gameState = new int[3];
+  boolean isX = true;
+
   public static void main(String[] args) {
     System.out.println("Hello, World!");
+  }
+
+  public resetGameState() {
+    for (int i = 0; i < 9; i++) {
+      gameState[i] = 0;
+    }
+  }
+
+  public getGameOver() {
+  }
+
+  public switchPlayer() {
+    isX = false;
+  }
+}
+
+public class Square {
+  public Square() {
+    this.value = " ";
+  }
+
+  playMove(String move) {
+    this.value = move;
   }
 }
